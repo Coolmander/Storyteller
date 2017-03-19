e9c22b209fc6ef881bbc3590c0e342fac8b6f299
Anja <Anja@DESKTOP-NCDR4PO.fritz.box>
2017-03-19 12:29:19 +0100

    create project

---
+++ storyteller/src/main/java/com/moelleranja/dsa/DsaStorytellerApplication.java
@@ -0,0 +1,12 @@
+package com.moelleranja.dsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaStorytellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsaStorytellerApplication.class, args);
+	}
+}
